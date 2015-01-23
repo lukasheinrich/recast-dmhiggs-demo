c041e7d352d9233e36e3779bba968fa31eee037e
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-01-23 15:43:03 +0100

    fix name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = '',
+  name = 'recast-dmhiggs-demo',
   version = '0.0.1',
   description = 'recast-dmhiggs-demo',
   url = 'http://github.com/lukasheinrich/recast-dmhiggs-demo',
