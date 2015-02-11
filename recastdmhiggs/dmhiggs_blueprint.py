1245bcd88849a1efda361eb5eeb1b26c6649313b
Lukas <lukas.heinrich@gmail.com>
2015-02-10 19:07:21 -0500

    Update dmhiggs_blueprint.py

--- recastdmhiggs/dmhiggs_blueprint.py
+++ recastdmhiggs/dmhiggs_blueprint.py
@@ -3,6 +3,9 @@ blueprint = Blueprint('dmhiggs_analysis', __name__, template_folder='dmhiggs_tem
 
 RECAST_ANALYSIS_ID = '858fb12c-b62f-9954-1997-a6ff8c27be0e'
 
+
+RECASTSTORAGEPATH = '/home/analysis/recast/recaststorage'
+
 import json
 import requests
 import dmhiggs_backendtasks
@@ -18,7 +21,7 @@ def efficiency(requestId,parameter_pt):
 
 @blueprint.route('/result/<requestId>/<parameter_pt>')
 def result_view(requestId,parameter_pt):
-  plotlist = [os.path.basename(p).rsplit('.',1)[0] for p in glob.glob('results/{}/{}/plots/DMHiggsFiducial/*.dat'.format(requestId,parameter_pt))]
+  plotlist = [os.path.basename(p).rsplit('.',1)[0] for p in glob.glob('{}/results/{}/{}/plots/DMHiggsFiducial/*.dat'.format(RECASTSTORAGEPATH,requestId,parameter_pt))]
   return render_template('dmhiggs_result.html',analysisId = RECAST_ANALYSIS_ID,requestId=requestId,parameter_pt=parameter_pt,plotlist = plotlist)
 
 
